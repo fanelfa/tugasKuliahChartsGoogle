47a6a46f03b97284b3b2ce80e4e45e1fae474a21
fanelfa <ilhamelfaro@gmail.com>
2020-12-12 21:05:52 +0700

    implement date range to input date

--- js/range.js
+++ js/range.js
@@ -1,13 +1,28 @@
 $(function () {
     $("#slider-range").slider({
         range: true,
-        min: 0,
-        max: 500,
-        values: [75, 300],
+        min: new Date('2010.01.01').getTime() / 1000,
+        max: new Date().getTime() / 1000,
+        step: 86400,
+        values: [new Date('2013.01.01').getTime() / 1000, new Date().getTime() / 1000],
         slide: function (event, ui) {
-            // $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-            console.log(ui.values);
+            // $("#amount").val((new Date(ui.values[0] * 1000).toDateString()) + " - " + (new Date(ui.values[1] * 1000)).toDateString());
+
+            var start = new Date(ui.values[0] * 1000);
+            var startDay = ("0" + start.getDate()).slice(-2);
+            var startMonth = ("0" + (start.getMonth() + 1)).slice(-2);
+
+            var startDate = start.getFullYear() + "-" + (startMonth) + "-" + (startDay);
+            $("#startDate").val(startDate);
+
+
+            var end = new Date(ui.values[1] * 1000);
+            var endDay = ("0" + end.getDate()).slice(-2);
+            var endMonth = ("0" + (end.getMonth() + 1)).slice(-2);
+
+            var endDate = end.getFullYear() + "-" + (endMonth) + "-" + (endDay);
+
+            $("#endDate").val(endDate);
         }
     });
-    // $("#amount").val("$" + $("#slider-range").slider("values", 0) + " - $" + $("#slider-range").slider("values", 1));
 });
